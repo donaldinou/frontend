5f33ca4f3cbc615772f1a4a322f4b23a875d9b09
Adrien Loyant <loyant@acreat.com>
2015-12-22 11:52:30 +0100

    norefs : *bugfix user from token manager factory*

--- src/Viteloge/FrontendBundle/Component/Token/ManagerFactory.php
+++ src/Viteloge/FrontendBundle/Component/Token/ManagerFactory.php
@@ -3,11 +3,15 @@
 namespace Viteloge\FrontendBundle\Component\Token {
 
     use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+    use Viteloge\CoreBundle\Entity\User;
 
     class ManagerFactory {
 
         public static function getManager(TokenStorageInterface $tokenStorage, $secret) {
             $user = $tokenStorage->getToken()->getUser();
+            if (!$user instanceof User) {
+                $user = null;
+            }
             return new Manager($secret, $user);
         }
     }
