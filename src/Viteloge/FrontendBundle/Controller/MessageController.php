b921e35c634c67c0b617f279ad2251b090104556
fmronan <fmronan@msn.com>
2016-10-17 10:01:31 +0200

    noref:send message with token

--- src/Viteloge/FrontendBundle/Controller/MessageController.php
+++ src/Viteloge/FrontendBundle/Controller/MessageController.php
@@ -92,7 +92,7 @@ namespace Viteloge\FrontendBundle\Controller {
                  if(is_null($this->getUser())){
                   $newuser = $this->get('viteloge_frontend_generate.user')->generate($message);
                   $message->setUser($newuser);
-                  $inscription = $this->inscriptionMessage($newuser);
+
                 }
 
                 $result = $this->sendMessage($message);
@@ -125,29 +125,6 @@ namespace Viteloge\FrontendBundle\Controller {
                         'VitelogeFrontendBundle:Message:email/message.html.twig',
                         array(
                             'message' => $message
-                        )
-                    ),
-                    'text/html'
-                )
-            ;
-            return $this->get('mailer')->send($mail);
-        }
-
-                /**
-         *
-         */
-        protected function inscriptionMessage(User $user) {
-            $trans = $this->get('translator');
-            $to = $user->getEmail();
-            $mail = \Swift_Message::newInstance()
-                ->setSubject($trans->trans('Votre compte sur viteloge.com'))
-                ->setFrom('contact@viteloge.com')
-                ->setTo($to)
-                ->setBody(
-                    $this->renderView(
-                        'VitelogeFrontendBundle:Contact:email/inscription.html.twig',
-                        array(
-                            'user' => $user
                         )
                     ),
                     'text/html'
